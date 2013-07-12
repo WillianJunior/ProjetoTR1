0f9b899f00d05ac64aae09a3a70577c84d5624c5
Willian <willian@willian-asus-laptop.(none)>
2013-07-11 21:20:45 -0300

    linf

---
+++ receiver.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "trans_protocols.h"
+
+int main () {
+
+	SendProt *receiver;
+	receiver = new StopNWait();
+
+	MSG_TYPE msg;
+	ACK_TYPE rnext = 0;
+
+	while(1) {
+		receiver->recvMsg(&msg, &rnext);
+		cout << "Received Message: " << (EXTRACT_MSG(msg)) << endl
+		<< "rnext: " << rnext << endl << endl;
+	}
+	
+}
